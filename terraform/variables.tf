80633f1024b7415d6041f09dcc06d6aef0ea0e46
S7iso <82435868+S7iso-Maseko@users.noreply.github.com>
2025-05-28 13:54:20 +0200

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aft_vpc_cidr" {}   
+
+variable "aft_vpc_private_subnet_01_cidr" {}   
+
+variable "aft_vpc_private_subnet_02_cidr" {}    
+
+variable "aft_vpc_public_subnet_01_cidr" {}    
+             
+variable "aft_vpc_public_subnet_02_cidr" {}  
