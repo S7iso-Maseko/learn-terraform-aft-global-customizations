f1b8fd7486666dede35de5eb3c7e52094e4b0307
S7iso <82435868+S7iso-Maseko@users.noreply.github.com>
2025-06-06 11:21:07 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,5 @@ variable "aft_vpc_private_subnet_02_cidr" {}
 variable "aft_vpc_public_subnet_01_cidr" {}    
              
 variable "aft_vpc_public_subnet_02_cidr" {}  
+
+variable "aft_vpc_public_subnet_03_cidr" {}
